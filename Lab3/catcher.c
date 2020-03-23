6a04753a49dca946eac794ee5ebe23ecb91d4c85
tarnaldo88 <tarnaldo88@gmail.com>
2020-03-23 13:28:45 -0700

    lab3 update

--- Lab3/catcher.c
+++ Lab3/catcher.c
@@ -19,17 +19,17 @@ static int count = 0, termCount = 0; //how many signals have been caught
 static const char signals [31][16] = {"HUP", "INT", "QUIT", "ILL", "TRAP", "ABRT", "BUS", "FPE", "KILL",
                                   "USR1", "EGV", "USR2", "PIPE", "ALRM", "TERM", "CHLD", "CONT", "STOP",
                                   "TSTP", "TTIN", "TTOU", "URG", "XCPU", "XFSZ", "VTALRM", "PROF",
-                                  "WINCH", "IO", "PWR", "SYS", "TMIN"};    
+                                  "WINCH", "IO", "PWR", "SYS"};    
 
 int main(int argc, char ** argv){    
     catcher(argc, argv);
     return 0;
 }
-
+//function to catch the signals
 void catcher(int argc, char **argv) {
     int i,x;    
     fprintf(stderr, "catcher: $$ = %d\n", getpid()); 
-
+    //nested loop to search through library of signals and determine which 
     for (i = 1; i < argc; i++){
         for(x = 0; x < 31; x++){
             if(strcmp(argv[i],signals[x]) == 0 ){
