81f5f91fec3348c0a7154f9fa0c9498ac2e984f2
tarnaldo88 <tarnaldo88@gmail.com>
2020-04-19 19:07:28 -0700

    updated makefile and launch phil

--- Lab4/dining-p.c
+++ Lab4/dining-p.c
@@ -17,32 +17,28 @@ The program is designed to be executed numerous times, simultaneously, to demons
 #include <signal.h>
 
 void termHandler(int sig);
-int randInt();
+void progCheck(int pos, int seats, int argc);
+void deallocate(int pos, sem_t chop[], char* sem);
 void dining(int argc, char **argv);
 void eat(int pos);
-void thinking (int pos);
+void think(int pos);
 
-static int end;
-static int next;
+static int end; //value to know when to end dining loop
 
 int main(int argc, char **argv){
   dining(argc, argv);
-  return 0;
+  return EXIT_SUCCESS;
 }
 
+//handles SIGTERM, and flips switch to end loop and end program
 void sigHandler(int sig) {
-	signal(SIGTERM, sigHandler);
-    printf("SIGTERM processed\n");
+	//signal(SIGTERM, sig);
+    printf("SIGTERM(%d) processed\n", sig);
 	end = 1;
 }
 
 void dining(int argc, char **argv){
-    //first check there are at least 2 arguments
-    if(argc != 3){
-		printf("Error: arguments < 2 \n");
-		exit(EXIT_FAILURE);
-	}
-	
+
     //keep track of the eat and think cycles
     int cycle = 0, i = 0;
     int pos = atoi(argv[2]);
@@ -52,29 +48,25 @@ void dining(int argc, char **argv){
 	int shm_fd;
 	char* sem = argv[2];
 
+	progCheck(pos,seats,argc);
+
     signal(SIGTERM, sigHandler);
-    setpgid();
-
-    //check if there is room at the table
-    if(!(pos <= seats)){
-		printf("Philosopher %d rejected due to table full\n", position);
-		exit(EXIT_FAILURE);
-	}
-
+	setpgid(getpid(), 0);    
+    
     for(; i < seats; i++){	
 		if(sem_init(&chop[i], 1, 1) < 0){
 			printf("Error when initializing semaphore\n");
 			exit(EXIT_FAILURE);
 		}
 	}
-
+	
     shm_fd = shm_open(argv[2], O_CREAT | O_EXCL, 0666);
 
     if(shm_fd < 0){
 		printf("Shared Memory Open failed\n");
 		exit(EXIT_FAILURE);
 	}
-    
+
     mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0); //memory map allocated
 
     //philosphers start dining
@@ -90,28 +82,35 @@ void dining(int argc, char **argv){
         cycle++;
 	} while (!end);
 
-    kill_cycle(pos,chop,sem);
+    deallocate(pos,chop,sem);
     fprintf(stderr, "Philosopher #%d completed %d cycles.\n", pos, cycle);
 }
 
-void eat(int var) {
+void progCheck(int pos, int seats, int argc){
+	//check there are at least 2 arguments
+	// also check there are not
+    if(argc != 3){
+		printf("Error: arguments < 2 \n");
+		exit(EXIT_FAILURE);
+	} else if(!(pos <= seats)){
+		printf("Philosopher %d rejected due to table full\n", pos);
+		exit(EXIT_FAILURE);
+	}	
+}
+
+void eat(int var){
 	printf("Philosopher #%d is eating\n", var);	
 	usleep(rand());
 }
 
-void think(int var) {
+void think(int var){
 	printf("Philosopher #%d is thinking\n", var);
 	usleep(rand());
 }
 
 //remove philospher from eat/think cycle and deallocate system resources
-void kill_cycle(int position, sem_t chop[], char* sem)
-{
-	sem_close(&chop[position]);
+void deallocate(int pos, sem_t chop[], char* sem){
+	sem_close(&chop[pos]);
 	shm_unlink(sem);
-	sem_destroy(&chop[position]);
-}
-
-int randInt(){
-
+	sem_destroy(&chop[pos]);
 }
