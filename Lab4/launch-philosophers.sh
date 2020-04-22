3b2f07c6d9b135e8c8a2ae06fbfec616ab0dc039
tarnaldo88 <tarnaldo88@gmail.com>
2020-04-22 12:31:00 -0700

    old update i forgot to push

--- Lab4/launch-philosophers.sh
+++ Lab4/launch-philosophers.sh
@@ -1,14 +1,28 @@
 #Arnaldo Torres
 #launch philosophers - a program that simulates a solution to the dining philosopher problem.  
 
-EXEC = dining-p
-SEATS = $1
+EXEC=dining-p
+SEATS=$1
+child=$!
+pids=()
 
-#for every seat make a philopher
-for (i =0; $i < $SEATS; i+=1)
 
+sigHandler(){
+	for((i=0; $i<$SEATS; i+=1))
+	do
+		kill -TERM ${pids[i]} 2>/dev/null #children are killed
+	done
+    	exit 0
+}
+
+trap 'sigHandler' TERM
+
+#for every seat(n) create a philosopher
+for ((i=0; $i<$SEATS; i+=1))
 do
-    ./$EXEC $SEATS $i &
-    echo "./$EXEC $SEATS $i"
 
+	./$EXEC $SEATS $i &
+
+	pids[${i}]=$! #pid of children
 done
+wait
