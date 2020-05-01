62c8f8180bac5a57a54f3ceedb039f8b9b0ae31b
tarnaldo88 <tarnaldo88@gmail.com>
2020-05-01 11:22:41 -0700

    update

--- Lab4/dining-p.c
+++ Lab4/dining-p.c
@@ -1,7 +1,6 @@
 /*
 Programmer: Arnaldo Torres
 4/20/20
-
 Description: a program that simulates a single dining philosopher.  
 The program is designed to be executed numerous times, simultaneously, to demonstrate the effect use of semaphores.
 */
@@ -15,13 +14,15 @@ The program is designed to be executed numerous times, simultaneously, to demons
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void termHandler(int sig);
 void progCheck(int pos, int seats, int argc);
-void deallocate(int pos, sem_t chop[], char* sem);
+void deallocate(char semName1[], char semName2[], sem_t* chop[]);
 void dining(int argc, char **argv);
-void eat(int pos);
-void think(int pos);
+void eating(int pos);
+void thinking(int pos);
 
 static int end; //value to know when to end dining loop
 
@@ -32,85 +33,105 @@ int main(int argc, char **argv){
 
 //handles SIGTERM, and flips switch to end loop and end program
 void sigHandler(int sig) {
-	//signal(SIGTERM, sig);
+	signal(SIGTERM, sig);
     printf("SIGTERM(%d) processed\n", sig);
 	end = 1;
 }
 
 void dining(int argc, char **argv){
-
     //keep track of the eat and think cycles
-    int cycle = 0, i = 0;
+    int cycle = 0, i = 0, semVal1, semVal2;
     int pos = atoi(argv[2]);
 	int seats = atoi(argv[1]);
-	const int SIZE = seats * 32;
-	sem_t chop[seats + 1];
-	int shm_fd;
-	char* sem = argv[2];
+	sem_t* chop[2], rtn1, rtn2;
+	char name[50], semName1[50], semName2[50]; //50 as an arbitrary number so nothing too random can overflow 
 
 	progCheck(pos,seats,argc);
-
     signal(SIGTERM, sigHandler);
-	setpgid(getpid(), 0);    
-    
-    for(; i < seats; i++){	
-		if(sem_init(&chop[i], 1, 1) < 0){
-			printf("Error when initializing semaphore\n");
-			exit(EXIT_FAILURE);
-		}
-	}
 	
-    shm_fd = shm_open(argv[2], O_CREAT | O_EXCL, 0666);
-
-    if(shm_fd < 0){
-		printf("Shared Memory Open failed\n");
-		exit(EXIT_FAILURE);
+	if(seats != 1){
+		sprintf(name, "%d", (pos % seats));
+	} else if (seats == 1){
+		sprintf(name, "%d", (pos+ 1));
 	}
 
-    mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0); //memory map allocated
+	strcpy(semName1, "/");
+	strcat(semName1, argv[2]);
+
+	strcpy(semName2, "/");
+	strcat(semName2, name);
+	
+    //start the left semaphore
+    rtn1 = sem_open(sem_name_1, O_CREAT | O_EXCL, 0666, 1);
+	if(rtn1 == SEM_FAILED){
+		rtn1 = sem_open(sem_name_1, 0);
+	}
+	//start the right semaphore
+	rtn2 = sem_open(sem_name_2, O_CREAT | O_EXCL, 0666, 1);
+	if(rtn2 == SEM_FAILED){
+		rtn2 = sem_open(sem_name_2, 0);
+	}
+	chop[0] = rtn1;
+	chop[1] = rtn2;
+
+    if(setpgid(getpid(), getppid()) != 0){
+		printf("Unable to get group id\n");
+		exit(EXIT_SUCCESS);
+	}
 
     //philosphers start dining
 	do {
-		sem_wait(&chop[pos]);
-		sem_wait(&chop[(pos + 1) % seats]);
-		eat(pos);
+		think(position);
+		sem_getvalue(chop[1], &semVal1);
+		sem_getvalue(chop[0], &semVal2);
 
-		sem_post(&chop[pos]);
-		sem_post(&chop[(pos + 1) % seats]);
-		think(pos);
+		//determine if left or right is free to go eat
+		if(semVal1 == 1 && semVal2 == 1){
 
+			eat(position);
+		//eating completed, post that eating is done
+			sem_post(chop[1]);
+			sem_post(chop[0]);
+		} else{
+			//wait for a free spot
+			sem_wait(chop[0]);
+			sem_wait(chop[1]);
+		}
         cycle++;
 	} while (!end);
 
-    deallocate(pos,chop,sem);
+    deallocate(semName1,semName2,chop);
     fprintf(stderr, "Philosopher #%d completed %d cycles.\n", pos, cycle);
 }
 
 void progCheck(int pos, int seats, int argc){
 	//check there are at least 2 arguments
-	// also check there are not
-    if(argc != 3){
+	if(argc != 3){
 		printf("Error: arguments < 2 \n");
 		exit(EXIT_FAILURE);
-	} else if(!(pos <= seats)){
+	} else if(!(pos <= seats)){ //make sure enough spots at the table
 		printf("Philosopher %d rejected due to table full\n", pos);
 		exit(EXIT_FAILURE);
 	}	
 }
 
-void eat(int var){
+void eating(int var){
 	printf("Philosopher #%d is eating\n", var);	
-	usleep(rand());
+	usleep(rand() % 888888);
 }
-
-void think(int var){
+void thinking(int var){
 	printf("Philosopher #%d is thinking\n", var);
-	usleep(rand());
+	usleep(rand() % 888888);
 }
 
 //remove philospher from eat/think cycle and deallocate system resources
-void deallocate(int pos, sem_t chop[], char* sem){
-	sem_close(&chop[pos]);
-	shm_unlink(sem);
-	sem_destroy(&chop[pos]);
+void deallocate(char semName1[], char semName2[], sem_t* chop[]){
+	sem_close(chop[0]);
+	sem_close(chop[1]);
+
+	sem_unlink(semName1);
+	sem_unlink(semName2);
+
+	sem_destroy(chop[0]);
+	sem_destroy(chop[1]);
 }
