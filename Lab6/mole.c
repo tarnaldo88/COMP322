4ed569a80ad1af32e3758399c58c668fabb5d4d1
tarnaldo88 <tarnaldo88@gmail.com>
2020-05-08 12:50:57 -0700

    update

---
+++ Lab6/mole.c
@@ -0,0 +1,31 @@
+/*
+Programmer: Arnaldo Torres
+Due Date: 5/8/20
+Program simply writes a signal line to a well-known log file (~/lab6.log)
+*/
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+void mole(int argc, char **argv);
+
+int main(int argc, char **argv) {
+    mole(argc, argv);
+    return 0;
+}
+
+void mole(int argc, char **argv){
+    FILE *filep = fopen("lab6.log", "a");
+    if (filep == NULL){
+        fprintf(stderr, "Error: failed to create Lab log");
+    }
+    if (filep != 0){
+        char * mole;
+        if (argc > 1 && strcmp(argv[1], "1") == 0)
+            mole = "Pop mole1";
+        else
+            mole = "Pop mole2";
+        fclose(filep);
+    }
+}
---
+++ Lab6/spiritd.c
@@ -0,0 +1,109 @@
+/*
+Programmer: Arnaldo Torres
+Due Date: 5/8/20
+Program spiritd (LAB6):program that is intended to run as a daemon, 
+and based upon signals will either create or destroy child processes.
+*/
+#include <stdio.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static pid_t mole1;
+static pid_t mole2;
+static int var;
+
+void randMole();
+static void sigHandler(int signum);
+void spirit();
+
+int main(int argc, char **argv){
+    spirit();
+    return EXIT_SUCCESS;
+}
+
+void spirit(){
+    pid_t dPID;
+    pid_t sid;
+    if ((dPID = fork()) < 0){
+        fprintf(stderr, "Error: fork has failed\n");
+    }
+    if (dPID != 0){        
+        printf("Daemon PID: %d\n", dPID);
+        exit(0); //parent is killed, daemon will run in background
+    }
+    if (dPID == 0) {
+        signal(SIGTERM, handler);
+        signal(SIGUSR1, handler);
+        signal(SIGUSR2, handler);
+        //Change file permissions of daemon
+        umask(0); 
+        if ((sid = setsid()) < 0){
+            fprintf(stderr, "setsid() failed\n");
+        }
+        int fd1 = open("/dev/null", O_RDONLY);
+        //changes the directory to root
+        if (chdir("/") != 0){ 
+            fprintf(stderr, "Error: chdir() has failed\n");
+        }
+        if (fd1 < 0){
+            fprintf(stderr, "Error: log has failed to open\n");
+        }
+        //redirecting stdout to the log file
+        if (dup2(fd1, STDOUT_FILENO) < 0){
+            fprintf(stderr, "Error: STDOUT failed to redirect\n");
+        }
+        //redirecting stderr to the log file
+        if (dup2(fd1, STDERR_FILENO) < 0){
+            fprintf(stderr, "Error: STDERR failed to redirect\n");
+        }
+        if (dup2(fd1, STDIN_FILENO) < 0){
+            fprintf(stderr, "Error: STDIN failed to redirect\n");
+        }
+        close(fd1);
+        while (1){
+            sleep(1);
+        }
+    }
+}
+
+static void sigHandler(int sig){
+    if(sig == SIGTERM){
+        kill(mole2, SIGTERM);
+        kill(mole1, SIGTERM);
+        exit(0);
+    } else if(sig == SIGUSR2){
+        randomMoleChoose();
+        (void) signal(SIGUSR2, handler);
+    } else if(sig == SIGUSR1){
+        randomMoleChoose();
+        (void) signal(SIGUSR1, handler);
+    }
+}
+
+void randMole(){
+    char moleNum[30];
+    var = rand() % 2;
+    sprintf(moleNum, "%d", var + 1);
+    char * mArgv[] = {"mole", moleNum, 0};
+    if (num == 0){
+        if (kill(mole1, SIGCHLD) < 0){
+            fprintf(stderr, "Process already killed");
+        }
+        mole1 = fork();
+        if (mole1 == 0) {
+            execv(mArgv[0], mArgv);
+        } else {
+            if (kill(mole2, SIGCHLD) < 0){
+                fprintf(stderr, "Process already killed");
+            }
+            mole2 = fork();
+            if (mole2 == 0){
+                execv(mArgv[0], mArgv);
+            }
+        }
+    }
+}
