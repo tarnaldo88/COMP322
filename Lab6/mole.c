ffb9a54697902523bd0ed4682bd277fbbf81371d
tarnaldo88 <tarnaldo88@gmail.com>
2020-05-13 12:53:13 -0700

    brackets format fix

--- Lab6/mole.c
+++ Lab6/mole.c
@@ -17,15 +17,17 @@ int main(int argc, char **argv) {
 
 void mole(int argc, char **argv){
     FILE *filep = fopen("lab6.log", "a");
+    char *
     if (filep == NULL){
         fprintf(stderr, "Error: failed to create Lab log");
     }
     if (filep != 0){
-        char * mole;
-        if (argc > 1 && strcmp(argv[1], "1") == 0)
-            mole = "Pop mole1";
-        else
-            mole = "Pop mole2";
+        char * m;
+        if (argc > 1 && strcmp(argv[1], "1") == 0){
+            m = "Pop mole1";
+        }else{
+            m = "Pop mole2";
+        }
         fclose(filep);
     }
 }
