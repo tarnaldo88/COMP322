270e1ace7396078e8843f1c04ec3a0c1efb02932
tarnaldo88 <tarnaldo88@gmail.com>
2020-05-13 15:57:13 -0700

    reworked to include proper fd declaration

--- Lab6/spiritd.c
+++ Lab6/spiritd.c
@@ -11,11 +11,11 @@ and based upon signals will either create or destroy child processes.
 #include <signal.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include<sys/resource.h>
 
 static pid_t mole;
 static pid_t mole2;
 static int var;
-
 
 static void sigHandler(int signum);
 void spirit();
@@ -26,7 +26,6 @@ int main(int argc, char **argv){
     spirit();
     return EXIT_SUCCESS;
 }
-
 
 static void sigHandler(int sig){
     char varChARzard[10];
@@ -44,14 +43,14 @@ static void sigHandler(int sig){
             fprintf(stderr,"SIGCHLD sent to %d\n",(int)mole2);
             kill(mole2,SIGCHLD);
         }
-        var = rand()%2;        
+        var = rand() % 2;        
         sprintf(varChARzard,"%d",var+1);
 
         mole2 = fork();
 
         if(mole2  == 0){
         fprintf(stderr,"Executing: %s , %s\n",shortcut[0],shortcut[1]);
-        execv(shortcut[0],args);
+        execv(shortcut[0],shortcut);
         fprintf(stderr,"Error: failed to execute, %s , %s\n",shortcut[0],shortcut[1]);
         }
         signal(SIGUSR1,sigHandler);
@@ -67,7 +66,7 @@ static void sigHandler(int sig){
         
         if(mole2 == 0){
             fprintf(stderr,"Executing: %s , %s\n",shortcut[0],shortcut[1]);
-            execv(shortcut[0],args);
+            execv(shortcut[0],shortcut);
             fprintf(stderr,"Error: failed to execute, %s , %s\n",shortcut[0],shortcut[1]);
         }
         signal(SIGUSR1,sigHandler);
@@ -86,13 +85,13 @@ void spirit(){
         fprintf(stderr,"Error: unable to fork \n");
     }
     if(mole > 0){
-        fprintf(stderr,"Mole PID: %d\n",pid);
+        fprintf(stderr,"Mole PID: %d\n",mole);
         exit(EXIT_FAILURE);
     }
     umask(0);
     //have to start a new session, then check that it did not fail
     sessionId = setsid();
-    if(sid < 0){
+    if(sessionId < 0){
         fprintf(stderr,"Error: failed to start new session\n");
         exit(EXIT_FAILURE);
     }
@@ -109,7 +108,7 @@ void spirit(){
         //takes in either mole 1 or 2 and also casts var as char, hence the new pokemon, varCHARzard
         sprintf(varChARzard,"%d",var+1);        
         printf("kill child process: %s\n ",shortcut[1]);
-        execv(shortcut[0],args);
+        execv(shortcut[0],shortcut);
         printf(stderr,"Error: unable to kill child process %s",shortcut[1]);
     }
 }
@@ -121,11 +120,11 @@ void fileLimits(){
     if(getrlimit(RLIMIT_NOFILE,&rLimit)<0){
         fprintf(stderr,"Error: unable to find file limits\n");
     }
-    if(rLimit.rlim_max==RLIM_INFINITY){
-        rLimit.rlim_max=1024;
+    if(rLimit.rlim_max == RLIM_INFINITY){
+        rLimit.rlim_max = 1024;
     }
     //for loop to close fd's
-    for(; i < rLimtrlim_max; i++){
+    for(; i < rLimit.rlim_max; i++){
         close(i);
     }
 }
@@ -138,9 +137,9 @@ void fdAndSigCatcher(){
     1 : STDOUT
     2 : STDERR
     */
-    fd0=open("/dev/null",O_RDWR);
-    fd1=dup(0);
-    fd2=dup(0);
+    fd = open("/dev/null",O_RDWR);
+    fd1 = dup(0);
+    fd2 = dup(0);
     //set signal catch for signal termination, and the user defined signals 1 and 2
     signal(SIGTERM, sigHandler);
     signal(SIGUSR1, sigHandler);
